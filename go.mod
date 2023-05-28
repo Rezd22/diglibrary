388ce090437aa62909dca04fa6df6546a82eedb6
Galang Prasanjaya <47128604+Rezd22@users.noreply.github.com>
2023-05-28 22:41:46 +0700

    first commit

--- go.mod
+++ go.mod
@@ -9,6 +9,11 @@ require (
 )
 
 require (
+	github.com/disintegration/imaging v1.6.2 // indirect
+	golang.org/x/image v0.7.0 // indirect
+)
+
+require (
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/cors v1.4.0
@@ -36,7 +41,7 @@ require (
 	golang.org/x/crypto v0.6.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
-	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"image"
+	"net/http"
+	"os"
+
+	_ "image/jpeg" // Import untuk mendukung format JPEG
+	_ "image/png"  // Import untuk mendukung format PNG
 	"log"
 	"paa/database"
 	"paa/handler"
@@ -10,10 +17,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// Book struct
+type Book struct {
+	Title    string
+	ImageURL string
+}
+
 var credDB = model.Cred{
 	Host:     "localhost",
 	User:     "postgres",
-	Password: "1377",
+	Password: "wswas321",
 	DBName:   "books",
 	Port:     5432,
 }
@@ -21,8 +34,29 @@ var credDB = model.Cred{
 func main() {
 	db, err := database.ConnectDB(credDB)
 	if err != nil {
-		log.Fatalf("error connecting database : %v", err)
+		log.Fatalf("error connecting database: %v", err)
 	}
+
+	file, err := os.Open("static/image/buku.jpg")
+	if err != nil {
+		fmt.Println("Terjadi kesalahan saat membuka file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Dekode gambar
+	img, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("Terjadi kesalahan saat mendekode gambar:", err)
+		return
+	}
+
+	// Menampilkan informasi gambar
+	bounds := img.Bounds()
+	width := bounds.Max.X
+	height := bounds.Max.Y
+	fmt.Println("Lebar gambar:", width)
+	fmt.Println("Tinggi gambar:", height)
 
 	repo := repository.NewBooksRepository(db)
 	handler := handler.NewBooksHandler(repo)
@@ -37,14 +71,16 @@ func main() {
 	r.POST("/register", handler.CreateUser)
 	r.POST("/login", handler.LoginUser)
 
-	//handler api book
+	// handler api book
 	r.POST("/book", handler.IsLogin, handler.CreateBook)
 	r.GET("/book", handler.IsLogin, handler.GetAllBooks)
 	r.POST("/book/:id", handler.IsLogin, handler.UpdateBook)
 	r.DELETE("/book/:id", handler.IsLogin, handler.DeleteBook)
 
 	// handler page auth
-	r.GET("/", handler.ShowLoginPage)
+	r.GET("/", HomeHandler)
+	r.GET("/daftar",DaftarHandler)
+	r.GET("/login", handler.ShowLoginPage)
 	r.GET("/register", handler.ShowRegisterPage)
 
 	// handler page book
@@ -52,5 +88,28 @@ func main() {
 	r.POST("/edit-book/:id", handler.IsLogin, handler.ShowEditBookPage)
 	r.POST("/delete-book/:id", handler.IsLogin, handler.DeletePage)
 
+	// Menampilkan pesan server berjalan
+	fmt.Println("Server berjalan di http://localhost:8080")
+
 	r.Run(":8080")
+}
+
+// HomeHandler adalah handler untuk halaman home ("/")
+func HomeHandler(c *gin.Context) {
+	books := []Book{
+		{Title: "Harry Potter", ImageURL: "/static/images/harry_potter.jpg"},
+		{Title: "The Great Gatsby", ImageURL: "/static/images/great_gatsby.jpg"},
+		// Tambahkan data buku lainnya di sini
+	}
+
+	c.HTML(http.StatusOK, "home.html", gin.H{
+		"title": "Home",
+		"books": books,
+	})
+}
+func DaftarHandler(c *gin.Context) {
+	c.HTML(http.StatusOK, "boks.html", gin.H{
+		"title": "Daftar Buku",
+
+	})
 }
